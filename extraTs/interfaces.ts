f57a3b12dbb22ff5df875cd5901844d6e0c17aff
PiyushDua <piyush.dua@successive.tech>
2019-01-24 12:24:33 +0530

    Adding changes in extra/interface.ts file.

---
+++ extraTs/interfaces.ts
@@ -0,0 +1,13 @@
+export interface IPermissions {
+  [getUser: string]: {
+    all: string[];
+    read: string[];
+    write: string[];
+    delete: string[];
+  };
+}
+
+export interface IUsers {
+  traineeEmail: string;
+  reviewerEmail: string;
+}
